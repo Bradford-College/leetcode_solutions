5abe5c91fe127e0093e7f1eecdf1d824bebae189
Your Name <you@example.com>
2024-10-03 15:18:57 +0100

    :)

---
+++ Easy/solution.py
@@ -0,0 +1,44 @@
+'''
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution,
+and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+'''
+
+
+def twosum(nums: list, target: int):
+    lenght = len(nums)
+    for i in range(lenght):
+        for j in range(1, lenght + i):
+            if nums[i] + nums[j] == target:
+                # print(f"success [{i}:{j}]")
+                return i, j
+
+
+def main():
+    print(twosum(nums=[2, 7, 11, 15], target=9))
+    print(twosum(nums=[3, 3], target=6))
+    print(twosum(nums=[3, 2, 4], target=6))
+
+
+main()
